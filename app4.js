9dc5523f923a793d38fea74e0072ea3f31e29d1c
Serhii <95491075+LevchukS@users.noreply.github.com>
2022-10-05 15:50:57 +0300

    Update app4.js

--- app4.js
+++ app4.js
@@ -1,53 +1,54 @@
-function appendRow(node) {
-    const span = document.createElement("SPAN");
-    const txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-  }
-  
-  function deleteRow(li) {
-      for (i = 0; i < close.length; i++) {
-          close[i].onclick = function() {
-            const div = this.parentElement;
-            div.style.display = "none";
-          }
-  }
-  }
-// Create a "close" button and append it to each list item
-const myNodelist = document.getElementsByTagName("li");
+const DOM = {
+  newTodoInput: document.getElementById('myInput'),
+  todosContainer: document.getElementById('myUL'),
+  addTodo: document.getElementById('addBtn'),
+};
 
-    Array.from(myNodelist).forEach(mode);{
-    appendRow(node);
-}
-
-// Click on a close button to hide the current list item
-const close = document.getElementsByClassName("close");
-deleteRow(li);
-
-// Add a "checked" symbol when clicking on a list item
-const list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'li') {
-    ev.target.classList.toggle('checked');
+const newCloseBtn = () => {
+  const closeBtn = document.createElement('span');
+  closeBtn.innerHTML = '&#x00d7; ';
+  closeBtn.classList.add('close');
+  closeBtn.onclick = () => {
+    closeBtn.parentElement.remove();
+  };
+  return closeBtn;
+};
+const handleCheckTodo = (todo) => {
+  todo.onclick = () => {
+    if (todo.classList.contains('checked')) {
+      todo.classList.remove('checked');
+    } else {
+      todo.classList.add('checked');
+    }
   }
-}, false);
+};
+const newTodo = () => {
+  const todo = document.createElement('li');
+  const todoText = document.createTextNode(DOM.newTodoInput.value);
+  const closeBtn = newCloseBtn();
 
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-    const li = document.createElement("li");
-    const inputValue = document.getElementById("myInput").value;
-    const t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
+  todo.appendChild(todoText);
+  todo.appendChild(closeBtn);
+  handleCheckTodo(todo);
+
+  DOM.todosContainer.appendChild(todo);
+  DOM.newTodoInput.value = '';
+};
+
+// validation
+DOM.addTodo.onclick = () => {
+  if (DOM.newTodoInput.value === '') {
+    alert('Please, fill the input!');
+    return;
   }
-  document.getElementById("myInput").value = "";
+  newTodo();
+};
 
-  appendRow(li);
-  deleteRow(li);
+// add check handlers, closeBtn to existing todos
+Array.from(DOM.todosContainer.children).forEach(todo => {
+  handleCheckTodo(todo);
+  todo.appendChild(newCloseBtn());
+});
 
   
 
